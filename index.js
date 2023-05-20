66af65699a8f44ac7f49db8e6a244f249113a0c3
RitaM5 <sporshia108514orva@gmail.com>
2023-05-21 03:24:13 +0600

    get search data impliment done

--- index.js
+++ index.js
@@ -28,12 +28,24 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // client.connect();
     const productsCollection = client.db('DisneyDollsDB').collection('products');
-
     app.get('/products', async (req, res) => {
       const limit = parseInt(req.query.limit) || 20;
       const cursor = productsCollection.find();
       const result = await cursor.limit(limit).toArray();
       res.send(result);
+    });
+    const searchToys = { toy_name: 1, category: 1 };
+    const searchOptions = { name: "toy_nameCategory" };
+    const result = await productsCollection.createIndex(searchToys, searchOptions);
+    app.get('/toysSearch/:text', async (req, res) => {
+      const searchText = req.params.text;
+      const result = await productsCollection.find({
+        $or: [
+          { toy_name: { $regex: searchText, $options: "i" } },
+          { category: { $regex: searchText, $options: "i" } },
+        ],
+      }).toArray();
+      res.send(result)
     });
     //for category
     app.get('/products/:text', async (req, res) => {
@@ -57,6 +69,26 @@ async function run() {
       res.send(result);
 
     });
+    //for my toys data
+    app.get('/sellerToys', async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      try {
+        if (req.query?.email) {
+          query = { email: req.query.email }
+          const sort = req.query?.sort == 'true' ? 1 : -1;
+          const result = await productsCollection.find(query).sort({ price: sort }).toArray();
+          res.send(result);
+        }
+        else {
+          res.status(404).send({ error: true, message: "Email not found" })
+        }
+      }
+      catch {
+        res.status(401).send({ error: true, message: "Unauthorized" })
+      }
+
+    })
     //for add toys
     app.post('/products', async (req, res) => {
       const addToys = req.body;
